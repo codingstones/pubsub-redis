0b2a9e255107c97fabf3f895b96b49757d0fbc6d
Néstor Salceda <nestor.salceda@gmail.com>
2017-09-11 10:57:02 +0200

    Create gem skeleton

---
+++ lib/pubsub/redis.rb
@@ -0,0 +1,7 @@
+require "pubsub/redis/version"
+
+module Pubsub
+  module Redis
+    # Your code goes here...
+  end
+end
---
+++ lib/pubsub/redis/version.rb
@@ -0,0 +1,5 @@
+module Pubsub
+  module Redis
+    VERSION = "0.1.0"
+  end
+end
